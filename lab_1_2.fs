e9b0999391dc402f513aeaa330046e7c7e61696d
Winnie <epanovaalina@gmail.com>
2026-02-24 18:25:50 +0500

    Add files via upload

---
+++ lab_1_2.fs
@@ -0,0 +1,22 @@
+﻿// Задание №2. Рекурсия
+open System
+let rec min_d n min =
+    if n = 0 then min
+    else
+        let digit = n % 10
+        let newMin = 
+            if digit < min 
+            then digit 
+            else min
+        min_d (n / 10) newMin
+
+
+[<EntryPoint>]
+let main args = 
+    printf "Введите число: "
+    let n = int (Console.ReadLine())
+    if n > 0 then
+        printf "Минимальная цифра: %d" (min_d n 9)
+    else
+        printfn "Ошибка! Число должно быть больше нуля."
+    0
