95358c05f8b3ba6af6cb2317a769680b194bf024
Winnie <epanovaalina@gmail.com>
2026-02-24 19:42:44 +0500

    Add files via upload

---
+++ lab_1_1.fs
@@ -0,0 +1,22 @@
+﻿// Лабораторная №1
+// Задание №1. Генерация списков
+open System
+
+let spisok (x : int) : int list =
+    [
+    for i in 1..x do
+        printf "Введите число %d: " i
+        let num = int (Console.ReadLine())
+        yield abs (num % 10)
+    ]
+
+
+[<EntryPoint>]
+let main args = 
+    printf "Введите количество чисел для ввода: "
+    let n = int (Console.ReadLine())
+    if n > 0 then
+        printf "Список последних цифр: %A " (spisok n) 
+    else
+        printfn "Ошибка! Количество должно быть положительным числом."
+    0
